1ee3e6f5b15f91966c12c0a9e7b4552453e9383d
Saishravan Muthukrishnan <saishravanm@gmail.com>
2024-02-24 17:04:17 -0600

    Redid Flask

--- backend/model.py
+++ backend/model.py
@@ -5,17 +5,25 @@ import numpy as np
 polyphen_radiation = []
 polyphen_pharma = []
 deceased_radiation_cases = []
+deceased_radiation_case_ids= []
 deceased_pharma_cases = []
+deceased_pharma_case_ids = []
 for x in radiation_cases:
     if(getattr(x,"days_to_death") != "'--"):
         #print(getattr(x,"days_to_death"))
         deceased_radiation_cases.append(x)
+        deceased_radiation_case_ids.append(getattr(x,'case_submitter_id'))
 for x in pharmatherapy_cases:
     if(getattr(x,"days_to_death") != "'--"):
         deceased_pharma_cases.append(x)
+        deceased_pharma_case_ids.append(getattr(x,'case_submitter_id'))
 
 for x in deceased_pharma_cases:
     print(getattr(x,'case_submitter_id'))
-for x in deceased_radiation_cases:
-    print(str(x))
-    print(radiation_treatment_list.get(getattr(x,"case_submitter_id")))
+
+p = deceased_radiation_cases[0]
+for x in dir(p):
+    print("<p2>",x.replace("_"," "),": ","{","{","patient.",x,"}","}","</p2>","\n","<br></br>")
+#for x in deceased_radiation_cases:
+#    print(str(x))
+#    print(radiation_treatment_list.get(getattr(x,"case_submitter_id")))
--- backend/website.py
+++ backend/website.py
@@ -1,12 +1,13 @@
 print("Content-Type: text/html\n")
 from flask import *
-from filter import *
+from model import *
 app = Flask(__name__)
 
 @app.route("/")
-def show_patmut():
-    return render_template('table.html',tables=[patmut.to_html(classes='patients')],titles=patmut.columns.values)
-
+#def show_patmut():
+#    return render_template('table.html',tables=[patmut.to_html(classes='patients')],titles=patmut.columns.values)
+def show_home():
+    return render_template('home.html',dec_rad_patnames=deceased_radiation_case_ids)
 @app.route("/radiation/<radpat>")
 def create_profile(radpat=None):
     patient = patient_list[radpat]
