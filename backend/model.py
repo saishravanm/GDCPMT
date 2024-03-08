7d03888fbfe07d0ade5de9f07b0a1565970fb568
Saishravan Muthukrishnan <saishravanm@gmail.com>
2024-03-08 12:42:12 -0600

    Generated Mutation Bar Graphs

--- backend/model.py
+++ backend/model.py
@@ -17,7 +17,30 @@ for x in pharmatherapy_cases:
     if(getattr(x,"days_to_death") != "'--"):
         deceased_pharma_cases.append(x)
         deceased_pharma_case_ids.append(getattr(x,'case_submitter_id'))
-
+def gen_radtreatment_to_mut_bar(x_n,y_n):
+    y_axis = []
+    x_axis = []
+    y_name = y_n
+    x_name = x_n #mutation
+    for x in radiation_treatment_list.values():
+        if(getattr(x,y_name) != "[Not Available]"):
+            z = patient_list.get(getattr(x,'case_submitter_id'))
+            x_axis.append(getattr(patient_to_mutation.get(z),x_name))
+            y_axis.append(getattr(x,y_name))
+    
+    fig, pat_to_rad_dose = plt.subplots()
+    #x_name.sort()
+    #y_name.sort()
+    pat_to_rad_dose.bar(x_axis,y_axis)
+    pat_to_rad_dose.set_ylabel(y_name.replace('_'," "))
+    pat_to_rad_dose.set_xlabel(x_name.replace('_'," "))
+    #print("x_name")
+    fig.set_size_inches(19.2,10.8)
+    plt.savefig("C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\models\\"+str(x_name + "_to_" + y_name)+".png",dpi=100)
+    plt.show()
+    
+    
+    
 def gen_radtreatment_bar(x_n, y_n):
     y_axis = []
     x_axis = []
@@ -35,10 +58,40 @@ def gen_radtreatment_bar(x_n, y_n):
     pat_to_rad_dose.bar(x_axis,y_axis)
     pat_to_rad_dose.set_ylabel(y_name.replace('_'," "))
     pat_to_rad_dose.set_xlabel(x_name.replace('_'," "))
-###################################################################################################
-
     plt.savefig("./models"+str(x_name + "_to_" + y_name)+".png")
     plt.show()
+###################################################################################################
+def gen_pharmatreatment_bar(x_n, y_n):
+    y_axis = []
+    x_axis = []
+    y_name = x_n
+    x_name = y_n
+    for x in pharma_treatment_list.values(): 
+        for z in x:
+         if(getattr(z,y_name) != "[Not Available]"):
+          y_axis.append(getattr(z,y_name))
+          x_axis.append(getattr(z,x_name))
+    #else:
+    #    deceased_radiation_case_ids.remove(deceased_radiation_case_ids.index(x))
+    fig, pat_to_rad_dose = plt.subplots()
+    x_name.sort()
+    y_name.sort()
+    pat_to_rad_dose.bar(x_axis,y_axis)
+    pat_to_rad_dose.set_ylabel(y_name.replace('_'," "))
+    pat_to_rad_dose.set_xlabel(x_name.replace('_'," "))
+    plt.savefig("./models"+str(x_name + "_to_" + y_name)+".png")
+    plt.show()
+
+#gen_pharmatreatment_bar('case_submitter_id','prescribed_dose')
+gen_radtreatment_to_mut_bar('impact','radiation_therapy_ended_days_to')
+gen_radtreatment_to_mut_bar('impact','radiation_total_dose')
+gen_radtreatment_to_mut_bar('impact','radiation_adjuvant_fractions_total')
+gen_radtreatment_to_mut_bar('impact','therapy_regimen')
+gen_radtreatment_to_mut_bar('impact','radiation_therapy_site')
+
+#gen_radtreatment_to_mut_bar('dna_change','radiation_therapy_site')
+
+
 
 #p = deceased_radiation_cases[0]
 #for x in dir(p):
--- backend/setup.py
+++ backend/setup.py
@@ -4,53 +4,53 @@ import pandas as pd
 clinical=pd.read_csv('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\clinical_data\\clinical.tsv',sep='\t')
 clinical.columns.name = 'clinical'
 
-exposure=pd.read_csv('./misc_data/exposure.tsv',sep='\t')
+exposure=pd.read_csv('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\misc_data\\exposure.tsv',sep='\t')
 exposure.columns.name = 'exposure'
 
-family_history=pd.read_csv('./misc_data/family_history.tsv',sep='\t')
+family_history=pd.read_csv('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\misc_data\\family_history.tsv',sep='\t')
 family_history.columns.name = 'family history'
 
-follow_up = pd.read_csv('./clinical_data/follow_up.tsv',sep='\t')
+follow_up = pd.read_csv('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\clinical_data\\follow_up.tsv',sep='\t')
 follow_up.columns.name = 'follow up'
 
-pathology_detail = pd.read_csv('./clinical_data/pathology_detail.tsv',sep='\t')
+pathology_detail = pd.read_csv('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\clinical_data\\pathology_detail.tsv',sep='\t')
 pathology_detail.columns.name = 'pathology detail'
 
 ##initalize variables to view biospecimen data
-aliquot=pd.read_csv('./misc_data/aliquot.tsv',sep='\t')
+aliquot=pd.read_csv('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\misc_data\\aliquot.tsv',sep='\t')
 aliquot.columns.name = 'aliquot'
 
-analyte=pd.read_csv('./biospecimen_data/analyte.tsv',sep='\t')
+analyte=pd.read_csv('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\biospecimen_data\\analyte.tsv',sep='\t')
 analyte.columns.name = 'analyte'
 
-portion=pd.read_csv('./clinical_data/portion.tsv')
+portion=pd.read_csv('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\clinical_data\\portion.tsv')
 portion.columns.name = 'portion'
 
-sample=pd.read_csv('./biospecimen_data/sample.tsv')
+sample=pd.read_csv('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\biospecimen_data\\sample.tsv')
 sample.columns.name = 'sample'
 
-slide=pd.read_csv('./biospecimen_data/slide.tsv')
+slide=pd.read_csv('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\biospecimen_data\\slide.tsv')
 slide.columns.name = 'slide'
 
-mutations1 = pd.read_csv('./mutations_data/mutations_1.csv',sep='\t')
+mutations1 = pd.read_csv('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\mutations_data\\mutations_1.csv',sep='\t')
 #mutations1.columns.name = 'mutations1'
 
-mutations2 = pd.read_csv('./mutations_data/mutations_2.csv')
+mutations2 = pd.read_csv('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\mutations_data\\mutations_2.csv')
 mutations2.columns.name = 'mutations2'
 
-mutations3 = pd.read_csv('./mutations_data/mutations_3.csv')
+mutations3 = pd.read_csv('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\mutations_data\\mutations_3.csv')
 mutations3.columns.name = 'mutations3'
 
-mutations = pd.read_excel('./mutations_data/mutations1.xlsx')
+mutations = pd.read_excel('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\mutations_data\\mutations1.xlsx')
 #mutations.columns.name = mutations
 
 #radiationinfo = pd.read_csv('./clinical_data/radiationinfo.tsv',delimiter='/t')
 
-treatment_data = pd.ExcelFile('./clinical_data/treatment_data.xlsx')
+treatment_data = pd.ExcelFile('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\clinical_data\\treatment_data.xlsx')
 radiation_treatment_data = pd.read_excel(treatment_data,'radiation_info')
 pharmaceutical_treatment_data = pd.read_excel(treatment_data,'pharma_info')
 
-with open('./mutations_data/patmut.txt','r',encoding='utf-8',newline='') as patmutcsv:
+with open('C:\\Users\\Sai\\Documents\\GitHub\\TPBase\\backend\\mutations_data\\patmut.csv','r',encoding='utf-8',newline='') as patmutcsv:
     patmut = pd.read_csv(patmutcsv)
 #print(str(patmut))
 print(type(patmut))
